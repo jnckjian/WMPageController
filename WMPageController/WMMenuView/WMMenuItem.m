fb53eb76ab4089e7a37225fb219b959c98303955
jnckjian <494625886@qq.com>
2019-07-08 16:36:48 +0800

    Update WMMenuItem.m

--- WMPageController/WMMenuView/WMMenuItem.m
+++ WMPageController/WMMenuView/WMMenuItem.m
@@ -83,7 +83,11 @@
     CGFloat g = _normalGreen + (_selectedGreen - _normalGreen) * rate;
     CGFloat b = _normalBlue + (_selectedBlue - _normalBlue) * rate;
     CGFloat a = _normalAlpha + (_selectedAlpha - _normalAlpha) * rate;
-    self.textColor = [UIColor colorWithRed:r green:g blue:b alpha:a];
+    NSMutableAttributedString *attr = [[NSMutableAttributedString alloc] initWithAttributedString:self.attributedText];
+    NSRange range = [attr.string rangeOfString:@"\n"];
+    [attr addAttribute:NSForegroundColorAttributeName value:[UIColor colorWithRed:r green:g blue:b alpha:a] range:NSMakeRange(0, range.location)];
+    [attr addAttribute:NSForegroundColorAttributeName value:[UIColor colorWithRed:r green:g blue:b alpha:a * 0.8] range:NSMakeRange(range.location, attr.string.length - range.location)];
+    self.attributedText = attr;
     CGFloat minScale = self.normalSize / self.selectedSize;
     CGFloat trueScale = minScale + (1 - minScale)*rate;
     self.transform = CGAffineTransformMakeScale(trueScale, trueScale);
