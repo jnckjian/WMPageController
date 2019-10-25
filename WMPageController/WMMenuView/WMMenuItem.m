ecc5fb1f859ae48710f5400f1789b131d7f69dc1
jnckjian <494625886@qq.com>
2019-10-25 14:46:47 +0800

    Update WMMenuItem.m

--- WMPageController/WMMenuView/WMMenuItem.m
+++ WMPageController/WMMenuView/WMMenuItem.m
@@ -84,6 +84,7 @@
     CGFloat b = _normalBlue + (_selectedBlue - _normalBlue) * rate;
     CGFloat a = _normalAlpha + (_selectedAlpha - _normalAlpha) * rate;
     NSMutableAttributedString *attr = [[NSMutableAttributedString alloc] initWithAttributedString:self.attributedText];
+    NSRange range = [attr.string rangeOfString:@"\n"];
     if (range.length > 0) {
         [attr addAttribute:NSForegroundColorAttributeName value:[UIColor colorWithRed:r green:g blue:b alpha:a] range:NSMakeRange(0, range.location)];
         [attr addAttribute:NSForegroundColorAttributeName value:[UIColor colorWithRed:r green:g blue:b alpha:a * 0.6] range:NSMakeRange(range.location, attr.string.length - range.location)];
