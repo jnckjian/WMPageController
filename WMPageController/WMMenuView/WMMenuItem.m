41503db72cd004271fbcdad4e6b445188bb93e11
jnckjian <494625886@qq.com>
2019-10-28 19:31:48 +0800

    Update WMMenuItem.m

--- WMPageController/WMMenuView/WMMenuItem.m
+++ WMPageController/WMMenuView/WMMenuItem.m
@@ -89,6 +89,9 @@
         [attr addAttribute:NSForegroundColorAttributeName value:[UIColor colorWithRed:r green:g blue:b alpha:a] range:NSMakeRange(0, range.location)];
         [attr addAttribute:NSForegroundColorAttributeName value:[UIColor colorWithRed:r green:g blue:b alpha:a * 0.6] range:NSMakeRange(range.location, attr.string.length - range.location)];
     }
+    else{
+        [attr addAttribute:NSForegroundColorAttributeName value:[UIColor colorWithRed:r green:g blue:b alpha:a] range:NSMakeRange(0, attr.length)];
+    }
     self.attributedText = attr;
     CGFloat minScale = self.normalSize / self.selectedSize;
     CGFloat trueScale = minScale + (1 - minScale)*rate;
